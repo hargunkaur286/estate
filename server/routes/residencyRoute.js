b008c36ca068dd9fba6b199d6458cd43098c7b05
Hargun Kaur <hargunkaur286@gmail.com>
2023-08-21 20:28:15 +0530

    authentication

--- server/routes/residencyRoute.js
+++ server/routes/residencyRoute.js
@@ -1,10 +1,11 @@
 import express from "express"
+import jwtCheck from "../config/auth0Config.js";
 import { createResidency } from "../controllers/residencyCntrl.js";
 import { getAllResidencies } from "../controllers/residencyCntrl.js";
 import { getResidency } from "../controllers/residencyCntrl.js";
 const router = express.Router()
 
-router.post("/create", createResidency)
+router.post("/create", jwtCheck,createResidency)
 router.get("/allresd", getAllResidencies);
 router.get("/:id", getResidency)
 
